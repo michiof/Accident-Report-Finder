b35d2ed60c81dc3eb2871e22dd11bfa04edc7505
Michio <133916533+michiof@users.noreply.github.com>
2023-10-29 17:56:31 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -140,13 +140,6 @@ def main():
     with st.sidebar:
         st.write('Version: 1.0.0')
         st.write('Made by [Michio Fujii](https://github.com/michiof)')
-        st.write('---')
-        st.write('A search tool for maritime accident reports using GPT and vector search technology.')
-        st.write('The program uses processed data from the maritime accident reports published by the Japan Transport Safety Board.')
-        st.write('The data consists of 14,875 records obtained in June 2023.')
-        st.write('14,875件の船舶事故報告書データを本プログラムの検索用に加工して利用しています。')
-        st.write('データ出典：[運輸安全委員会](https://jtsb.mlit.go.jp/jtsb/ship/index.php)')
-
 
     # メッセージのリストを維持する
     if 'messages' not in st.session_state:
